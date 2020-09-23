0e119fbdca3a1cefec5a921a36242447876f6291
tomsh5 <tomsh5@gmail.com>
2020-09-23 22:43:00 +0300

    colors

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-	//dbURL: "mongodb://localhost:27017",
+	//dbURL: "mongodb://localhost:27017", ff
 	"dbURL": "mongodb+srv://tomsh5:9UaPzFXMluf39ZVW@cluster0.mo1ni.mongodb.net/zoomify?retryWrites=true&w=majority",
 };
